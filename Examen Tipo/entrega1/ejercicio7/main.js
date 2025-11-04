689b9bd56884dae4c282e718be86c0203a34af1d
Rubencalii <rubencorralromero2018@gmail.com>
2025-11-04 08:55:05 +0100

    ejercicio correguido

--- Examen Tipo/entrega1/ejercicio7/main.js
+++ Examen Tipo/entrega1/ejercicio7/main.js
@@ -1,13 +1,14 @@
-(/* Ejercicio 7 */
+/* Ejercicio 7 */
 
 let arrayNumeros = [];
 
 // Pedir 10 números por pantalla
-for(let i= 0; i < 10; i++){
-    do{
-        let numString = prompt(`Introduce el número ${i+1} de 10 `);
-        let num = parseInt(numString);
-    }while(isNaN(num))
+for (let i = 0; i < 10; i++) {
+    let num;
+    do {
+        const numString = prompt(`Introduce el número ${i + 1} de 10 `);
+        num = parseInt(numString);
+    } while (isNaN(num));
     arrayNumeros.push(num);
 }
 
@@ -15,26 +16,23 @@ console.log(arrayNumeros);
 
 // Pedir las posiciones inicial y final y comprobar que son correctas
 let inicial = 0, final = 0;
-do{
+do {
     alert("Los valores inicial y final tienen que estar entre 0 y 9, e inicial tiene que ser menor que final");
-    do{
-        let inicialString = prompt(`Introduce la posición inicial `);
+    do {
+        const inicialString = prompt(`Introduce la posición inicial `);
         inicial = parseInt(inicialString);
-    }while(isNaN(inicial))
+    } while (isNaN(inicial));
 
-    do{
-        let finalString = prompt(`Introduce la posición final `);
+    do {
+        const finalString = prompt(`Introduce la posición final `);
         final = parseInt(finalString);
-    }while(isNaN(final))
-}while(inicial < 0 || inicial > 9 || final < 0 || final > 9 || inicial > final)
+    } while (isNaN(final));
+} while (inicial < 0 || inicial > 9 || final < 0 || final > 9 || inicial >= final);
 
 // Realizamos el desplazamiento del array
-let valorInicial = arrayNumeros[inicial];
-
-let arrayFinal = [...arrayNumeros];
-arrayFinal.splice(final, 0, valorInicial);
-let ultimoelemento = arrayFinal.pop();
-arrayFinal.unshift(ultimoelemento);
-arrayFinal.splice(inicial + 1, 1);
+// Realizamos el desplazamiento del array: retiramos el elemento en 'inicial' y lo insertamos en 'final'
+const arrayFinal = [...arrayNumeros];
+const [valor] = arrayFinal.splice(inicial, 1); // elimina y recoge el elemento
+arrayFinal.splice(final, 0, valor); // inserta en la posición final
 
 console.log(arrayFinal);
